9279253f92570b8c6e856107bcee2b29b54aac49
Shashwat-Nautiyal <shash01nautiyal@gmail.com>
2025-12-31 02:33:08 +0530

    harcode req json for rpc calls

---
+++ NOTE.md
@@ -0,0 +1,16 @@
+##Stage 1 invariants (lock these mentally)
+
+- Lossless
+Must be able to replay the trace byte for byte later.
+
+- Streaming only
+No serde_json::Value, no full deserialization (as it can cause OOM).
+
+- Client agnostic at the boundary
+trace-rpc crate should act as a translation layer. For an new client we just need to write a new adapter and not the entire engine.
+
+- Deterministic artifacts
+Same tx, same node, same output bytes.
+
+## Learnings
+- Expose artifacts instead of json sturctures. As soon as you get the JSON from the node, immediately turn it into your own clean custom Rust types.
--- crates/trace-rpc/src/lib.rs
+++ crates/trace-rpc/src/lib.rs
@@ -1,14 +1,58 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use std::path::PathBuf;
+
+// one fully acquired tx trace
+pub struct RawTrace {
+    pub tx_hash: String,
+    pub trace_path: PathBuf,  // saved raw exec trace from debug_traceTransaction
+    pub receipt_path: PathBuf,  // from eth_getTransactionReceipt
+    pub metadata_path: PathBuf,
+}
+
+// configuration for acquiring traces
+pub struct TraceConfig {
+    pub rpc_url: String,
+    out_dir: PathBuf,
+}
+
+pub fn debug_trace_payload(tx_hash: String) -> String {
+    // raw string
+    format!{
+        r#"{{
+            "jsonrpc": "2.0",
+            "id": "1",
+            "method": "debug_traceTransaction",
+            "params": [
+                "{}",
+                {{
+                    "disableStack": false,
+                    "disableMemory": true,
+                    "disableStorage": true,
+                }}
+            ]
+        }}"#,
+        tx_hash
+    }
+}
+
+pub fn receipt_payload(tx_hash: String) -> String{
+    format!(
+        r#"
+            {{
+                "jsonrpc": "2.0",
+                "id": "1",
+                "method": "eth_getTransactionReceipt",
+                "params": ["{}"]
+
+            }}
+        "#,
+        tx_hash
+    )
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    
+   
 }
